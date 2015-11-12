b8a69055f3cc200e6c200662111abf6f49ec2eca
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2015-11-12 16:13:52 +0000

    Modificando

--- quiz/controllers/quiz_controller.js
+++ quiz/controllers/quiz_controller.js
@@ -2,9 +2,6 @@
 
 exports.question = function(req, res) {
 	res.render('quizes/question', {pregunta: 'Capital de Italia'});
-	res.render('quizes/question', {pregunta1: '¿En qué deporte se usa tiza?'});
-	res.render('quizes/question', {pregunta2: '¿Cuántas manos tiene un caballo?'});
-	res.render('quizes/question', {pregunta3: '¿Qué órgano segrega la hormona insulina?'});
 };
 
 // GET /quizes/answer
@@ -16,12 +13,5 @@ exports.answer = function(req, res) {
 	else
 	{
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
-	if (req.query.respuesta1 === 'Billar'){
-		res.render('quizes/answer', {respuesta1: 'Correcto' });	
-	}
-	else
-	{
-		res.render('quizes/answer', {respuesta1: 'Incorrecto'});
 	}
 };
